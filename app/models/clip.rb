eb47e1649cf295abbe903a4cb6cad91da07c920d
kaifius <kai.hofius@gmail.com>
2014-02-24 11:36:01 -0800

    installed sunspot, annotated models

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: clips
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  user_id     :integer
+#  transcript  :text
+#  audio       :string(255)
+#  description :text
+#  private     :boolean
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Clip < ActiveRecord::Base
   belongs_to :user
 end
