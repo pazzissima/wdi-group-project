5bc0a4a03a675790ca0a20beebd958df93718dc6
JSchulhof <JSchulhof@gmail.com>
2014-02-25 14:49:40 -0800

    working intro email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,13 +28,15 @@ WdiGroupProject::Application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
      :address => "smtp.gmail.com",
      :port => 587,
+     :domain => 'gmail.com',
      :authentication => "plain", 
      :enable_starttls_auto => true,  
-     :user_name => 'myemail@gmail.com',
-     :password => 'mypassword'
+     :user_name => 'musicclips111@gmail.com',
+     :password => ENV['EMAIL_NAME']
    } 
 end
