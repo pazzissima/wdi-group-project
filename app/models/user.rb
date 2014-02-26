c9b905ff3026befe24662b9143be8b738be8ee71
kaifius <kai.hofius@gmail.com>
2014-02-26 11:16:27 -0800

    fixed likes and disabled :confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,7 @@ class User < ActiveRecord::Base
 
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable #, :confirmable
 
 end
 
