3a54544f786a6a85c882a0557d9c13031fb93171
melodicodyssey <stevedbauer@gmail.com>
2014-02-27 21:03:59 -0800

    implemented JS csrf fix

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -17,11 +17,15 @@ $(".like").on('click', function(event){
 	// this.closest.attr("id") should === the clip id
 	var id = $(this).closest("div").attr("id").substr(4);
 
+	var authParam = $('meta[name=csrf-param]').attr('content');
+  var authToken = $('meta[name=csrf-token]').attr('content');
+  var data = {};
+  data[authParam] = authToken;
 
 	$.ajax({
 		url: "/clips/update/"+id+".json",
 		type: "POST",
-		data: {'op' : 'like'},
+		data: data,
 		success: function(response) {
 			// call the function from Likes
 			Likes.increment_likes_in_DOM(response);
